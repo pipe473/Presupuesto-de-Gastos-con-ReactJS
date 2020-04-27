4b2cab27a1b9daad3c2ac542e5ca9b9659fa869b
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-04-28 01:00:05 +0200

    Documentando el componente de Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Error from './Error';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import Error from './Error';
 
 
 const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
@@ -75,6 +76,11 @@ const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
 
         </form>
      );
+}
+
+Formulario.propTypes = {
+    Guardargasto: PropTypes.func.isRequired,
+    GuardarCreargasto: PropTypes.func.isRequired
 }
  
 export default Formulario;
