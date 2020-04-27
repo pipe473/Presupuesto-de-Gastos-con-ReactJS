5510e1e815e02c5e6f156b6a43c1b839465f9af5
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-04-27 23:45:27 +0200

    Creando componente que mostrara presupuesto y el restante

---
+++ src/components/controlPresupuesto.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const controlPresupuesto = () => {
+    return ( 
+        <Fragment>
+            <div className="alert alert-primary">
+                
+            </div>
+        </Fragment>
+     );
+}
+ 
+export default controlPresupuesto;
